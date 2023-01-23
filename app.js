59286a0b5f0a0b61aa218f8b7446497390570b3c
luntik0011 <luntik0011@ya.ru>
2023-01-23 20:04:19 +0300

    Обработка ошибок

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ const http = require('http');
 
 //Старт бота//////////////////////////////////////////////
 const bot = new Telegraf('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
-bot.launch();
+bot.launch()
+    .catch((err) => {
+        console.log('Error: ', err.message)
+    });
 
 let Myip;
 let timerId = setInterval(GetIP, 5*60*1000,);
+console.log("Успешный запуск!");
+GetIP();
 
 function GetIP(){
     http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'},  function(resp) {
         resp.on('data', function(ip) {
-            // console.log("My public IP address is: " + ip);
+            console.log("My public IP address is: " + ip);
             if (Myip != ip) {
-                bot.telegram.sendMessage('XXXXXXXXXXXX', `My public IP address is:\n` + ip);
-                Myip = String(ip);
-            }
+                    bot.telegram.sendMessage('XXXXXXXXXXXX', `My public IP address is:\n` + ip)
+                    .catch((err) => {
+                        console.log('Error: ', err.message)
+                    });
+                    Myip = String(ip);
+                }
         });
+    }).on('error', function(err) {
+        console.log("Error: " + err.message);
     });
 };
